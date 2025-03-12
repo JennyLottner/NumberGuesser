8a4cec15a277193644a7fa612972e122942a3343
JennyLottner <jenny.lottner@gmail.com>
2025-03-12 16:07:41 +0200

    Add toggle instructions-modal state and function

--- src/app/cmps/app-header/app-header.component.ts
+++ src/app/cmps/app-header/app-header.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './app-header.component.html',
-  styleUrl: './app-header.component.scss'
+  styleUrl: './app-header.component.scss',
 })
 export class AppHeaderComponent {
+  isInstructionsModal: boolean = false; // Track instructions modal display
 
+  toggleModal(): void { // Toggle instructions modal display
+    this.isInstructionsModal = !this.isInstructionsModal;
+  }
 }
