efec3f92af972283edc67a909daeb6ed0bec0095
JennyLottner <jenny.lottner@gmail.com>
2025-03-12 16:06:06 +0200

    Create instructions-modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './cmps/app-header/app-header.component';
 import { AppFooterComponent } from './cmps/app-footer/app-footer.component';
+import { InstructionsModalComponent } from './cmps/instructions-modal/instructions-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppHeaderComponent,
-    AppFooterComponent
+    AppFooterComponent,
+    InstructionsModalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cmps/instructions-modal/instructions-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'instructions-modal',
+  templateUrl: './instructions-modal.component.html',
+  styleUrl: './instructions-modal.component.scss'
+})
+export class InstructionsModalComponent {
+
+}
