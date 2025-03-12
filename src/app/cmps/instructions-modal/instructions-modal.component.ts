fd2dd7b20f5046a957017e270b700f6ad447f865
JennyLottner <jenny.lottner@gmail.com>
2025-03-12 16:12:36 +0200

    Add output and function to instructions-modal

--- src/app/cmps/instructions-modal/instructions-modal.component.ts
+++ src/app/cmps/instructions-modal/instructions-modal.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'instructions-modal',
   templateUrl: './instructions-modal.component.html',
-  styleUrl: './instructions-modal.component.scss'
+  styleUrl: './instructions-modal.component.scss',
 })
 export class InstructionsModalComponent {
+  @Output() close = new EventEmitter<void>();
 
+  closeModal(): void { // Emitting event to parent to close modal
+    this.close.emit();
+  }
 }
